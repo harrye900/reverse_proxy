938d420dc1166065863fe651f2e405f3d28529cb
harry <harifosa@gmail.com>
2025-11-19 15:39:53 -0500

    Simplify auth-service Program.cs

--- ecommerce-app/microservices/auth-service/Program.cs
+++ ecommerce-app/microservices/auth-service/Program.cs
@@ -1,9 +1,6 @@
-using AuthService.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddSingleton<AuthenticationService>();
 
 builder.Services.AddCors(options =>
 {
