05c52aa98a460372ea59d7d9b150b5d1177d9a17
harry <harifosa@gmail.com>
2025-11-19 20:08:44 -0500

    Add web.config for payment service

--- ecommerce-app/microservices/payment-service/obj/Debug/net8.0/PaymentService.AssemblyInfo.cs
+++ ecommerce-app/microservices/payment-service/obj/Debug/net8.0/PaymentService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaymentService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eba45c0a75ade42d458697a5176c3d71370e873e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaymentService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaymentService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
