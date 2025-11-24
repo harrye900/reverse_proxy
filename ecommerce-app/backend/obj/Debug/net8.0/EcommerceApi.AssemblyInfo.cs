8b2e139d574ae990f4d61bc7ed99425748affabb
harry <harifosa@gmail.com>
2025-11-24 11:52:06 -0500

    update

--- ecommerce-app/backend/obj/Debug/net8.0/EcommerceApi.AssemblyInfo.cs
+++ ecommerce-app/backend/obj/Debug/net8.0/EcommerceApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76fcf1867c025793e1178117dc3127cc2787b22c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0996c9b4ae41c650a167bfab206edf870a79201b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
