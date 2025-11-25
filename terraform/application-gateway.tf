db2c46f3bb75af801fff3ad69af7b053c7f10428
harry <harifosa@gmail.com>
2025-11-24 21:27:10 -0500

    Disable nginx ingress, prepare for Application Gateway

---
+++ terraform/application-gateway.tf
@@ -0,0 +1,148 @@
+# Configure Azure Provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "b1d9dfac-423b-47ef-ba8b-e5d4c2050500"
+  features {}
+}
+
+# Get existing resource group
+data "azurerm_resource_group" "main" {
+  name = "rg-ecommerce-aks"
+}
+
+# Create public IP for Application Gateway
+resource "azurerm_public_ip" "app_gateway" {
+  name                = "pip-appgw-ecommerce"
+  resource_group_name = data.azurerm_resource_group.main.name
+  location            = data.azurerm_resource_group.main.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  
+  tags = {
+    Environment = "production"
+    Project     = "ecommerce"
+  }
+}
+
+# Create virtual network for Application Gateway
+resource "azurerm_virtual_network" "app_gateway" {
+  name                = "vnet-appgw-ecommerce"
+  resource_group_name = data.azurerm_resource_group.main.name
+  location            = data.azurerm_resource_group.main.location
+  address_space       = ["10.1.0.0/16"]
+  
+  tags = {
+    Environment = "production"
+    Project     = "ecommerce"
+  }
+}
+
+# Create subnet for Application Gateway
+resource "azurerm_subnet" "app_gateway" {
+  name                 = "snet-appgw"
+  resource_group_name  = data.azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.app_gateway.name
+  address_prefixes     = ["10.1.1.0/24"]
+}
+
+# Get AKS cluster details
+data "azurerm_kubernetes_cluster" "main" {
+  name                = "aks-ecommerce-cluster"
+  resource_group_name = data.azurerm_resource_group.main.name
+}
+
+# Application Gateway
+resource "azurerm_application_gateway" "main" {
+  name                = "appgw-ecommerce"
+  resource_group_name = data.azurerm_resource_group.main.name
+  location            = data.azurerm_resource_group.main.location
+
+  sku {
+    name     = "Standard_v2"
+    tier     = "Standard_v2"
+    capacity = 2
+  }
+
+  gateway_ip_configuration {
+    name      = "appgw-ip-config"
+    subnet_id = azurerm_subnet.app_gateway.id
+  }
+
+  frontend_port {
+    name = "http-port"
+    port = 80
+  }
+
+  frontend_port {
+    name = "https-port"
+    port = 443
+  }
+
+  frontend_ip_configuration {
+    name                 = "appgw-frontend-ip"
+    public_ip_address_id = azurerm_public_ip.app_gateway.id
+  }
+
+  # Backend pools
+  backend_address_pool {
+    name = "frontend-pool"
+  }
+
+  backend_address_pool {
+    name = "backend-pool"
+  }
+
+  # HTTP settings
+  backend_http_settings {
+    name                  = "frontend-http-settings"
+    cookie_based_affinity = "Disabled"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 60
+  }
+
+  backend_http_settings {
+    name                  = "backend-http-settings"
+    cookie_based_affinity = "Disabled"
+    port                  = 5000
+    protocol              = "Http"
+    request_timeout       = 60
+    path                  = "/api"
+  }
+
+  # HTTP listener
+  http_listener {
+    name                           = "http-listener"
+    frontend_ip_configuration_name = "appgw-frontend-ip"
+    frontend_port_name             = "http-port"
+    protocol                       = "Http"
+  }
+
+  # Request routing rules
+  request_routing_rule {
+    name                       = "frontend-rule"
+    rule_type                  = "Basic"
+    http_listener_name         = "http-listener"
+    backend_address_pool_name  = "frontend-pool"
+    backend_http_settings_name = "frontend-http-settings"
+    priority                   = 100
+  }
+
+  tags = {
+    Environment = "production"
+    Project     = "ecommerce"
+  }
+}
+
+# Output the public IP
+output "application_gateway_public_ip" {
+  value = azurerm_public_ip.app_gateway.ip_address
+}
